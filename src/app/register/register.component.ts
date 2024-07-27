01087b078c8e454aa2ce2c909d93faa9e14a59c2
unknown <danghieu1014@gmail.com>
2024-07-27 10:42:55 +0700

    validate register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
+  phone: string;
+  password: string;
+  repeatPassword: string;
+  fullName: string;
+  address: string;
+  isAccepted: boolean;
+  dateOfBirth: Date;
 
+  constructor() {
+    this.phone = '';
+    this.password = '';
+    this.repeatPassword = '';
+    this.fullName = '';
+    this.address = '';
+    this.isAccepted = false;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+  }
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phone}`);
+  }
+  register() {
+    const message =
+      `phone: ${this.phone}` +
+      `password: ${this.password}` +
+      `repeatPassword: ${this.repeatPassword}` +
+      `address: ${this.address}` +
+      `fullName: ${this.fullName}` +
+      `isAccepted: ${this.isAccepted}` +
+      `dateOfBirth: ${this.dateOfBirth}`;
+    alert(message);
+  }
+  checkPasswordMatch() {
+    if (this.password !== this.repeatPassword) {
+      this.registerForm.form.controls['repeatPassword'].setErrors({
+        passwordMismatch: true,
+      });
+    } else {
+      this.registerForm.form.controls['repeatPassword'].setErrors(null);
+    }
+  }
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (
+        monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())
+      ) {
+        age--;
+      }
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({
+          invalidAge: true,
+        });
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
+  }
 }
