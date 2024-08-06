62f62a763d0e3706cef513be1370525e02e35925
unknown <danghieu1014@gmail.com>
2024-08-06 15:31:10 +0700

    add login to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserResponse } from 'src/app/responses/user/user.response';
+import { TokenService } from 'src/app/service/token.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  userResponse?: UserResponse | null;
+  isPopoverOpen = false;
+
+  constructor(
+    private userService: UserService,
+    private tokenService: TokenService,
+
+  ) {}
+
+  togglePopover(event: Event): void {
+    event.preventDefault();
+    this.isPopoverOpen = !this.isPopoverOpen;
+  }
+  
+  handleItemClick(index: number): void {
+    if(index === 2) {
+      this.userService.removeUserFromLocalStorage();
+      this.tokenService.removeToken();
+      this.userResponse = this.userService.getUserResponseFromLocalStorage();
+    }
+    this.isPopoverOpen = false;
+  }
+
+  ngOnInit(): void {
+    this.userResponse = this.userService.getUserResponseFromLocalStorage();
+  }
 
 }
